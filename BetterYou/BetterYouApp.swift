8417fc9fa416391ebf75795f385fcaa501a2a600
Ricky <sparx3r@gmail.com>
2023-09-22 19:01:29 -0700

    Initial Commit

---
+++ BetterYou/BetterYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterYouApp.swift
+//  BetterYou
+//
+//  Created by Ricky Truckner on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterYou/PieData.swift
@@ -0,0 +1,20 @@
+//
+//  PieData.swift
+//  BetterYou
+//
+//  Created by Ricky Truckner on 9/22/23.
+//
+
+
+
+import Foundation
+
+struct SleepDataPoint: Identifiable {
+    
+    var cal: Int
+    var fat: Int
+    var sugar: Int
+    var protein: Int
+    var carb: Int
+}
+
