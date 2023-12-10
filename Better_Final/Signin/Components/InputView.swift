31f2d9932bc0de56101c9e9a8c9e985723f79bac
Ricky-Truckner <70228416+Ricky-Truckner@users.noreply.github.com>
2023-12-09 17:01:29 -0800

    Add files via upload

---
+++ Better_Final/Signin/Components/InputView.swift
@@ -0,0 +1,39 @@
+//
+//  InputView.swift
+//  caloriesApp
+//
+//  Created by Danca Nguyen on 12/6/23.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    @Binding var text: String
+    let title: String
+    let placeholder: String
+    var isSecureField = false
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            Text(title)
+                .foregroundStyle(Color(.darkGray))
+                .fontWeight(.semibold)
+                .font(.footnote)
+            
+            if isSecureField {
+                SecureField(placeholder, text: $text)
+                    .font(.system(size: 14))
+            }else {
+                TextField(placeholder, text: $text)
+                    .font(.system(size : 14))
+            }
+            Divider()
+        }
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView(text: .constant(""), title: "Email Address", placeholder: "name@example.com")
+    }
+}
